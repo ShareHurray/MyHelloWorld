ada868a62d1c7f4a3fd717de55092150cccd5816
ShareHurray <wakaowow@sina.com>
2013-06-29 15:50:03 +0800

    add some setter.just a test.

--- testwowedkio/src/testwowedkio/HelloWorld.java
+++ testwowedkio/src/testwowedkio/HelloWorld.java
@@ -6,4 +6,19 @@ public class HelloWorld {
 	private String address;
 	private String gender;
 	private String age;
+	public void setId(String id){
+		this.id = id;
+	}
+	public void setName(String name){
+		this.name = name;
+	}
+	public void setAddress(String address){
+		this.address = address;
+	}
+	public void setGender(String gender){
+		this.gender = gender;
+	}
+	public void setAge(String age){
+		this.age = age;
+	}
 }
